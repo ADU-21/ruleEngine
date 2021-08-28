22e10bc8916b9d4485b0e5dab5ee15d4344dde50
ADU <LukeDu571@gmail.com>
2021-08-28 11:29:05 -0700

    Init

---
+++ RuleProcessor/src/main/java/com/adu21/rule/engine/processor/RuleProcessor.java
@@ -0,0 +1,11 @@
+package com.adu21.rule.engine.processor;
+
+/**
+ * @author LukeDu
+ * @date 2021/8/28
+ */
+public class RuleProcessor {
+    public void sayHello() {
+        System.out.println("hello");
+    }
+}
---
+++ RuleReader/src/main/java/com/adu21/rule/engine/reader/RuleReader.java
@@ -0,0 +1,13 @@
+package com.adu21.rule.engine.reader;
+
+import com.adu21.rule.engine.processor.RuleProcessor;
+
+/**
+ * @author LukeDu
+ * @date 2021/8/28
+ */
+public class RuleReader {
+    public static void main(String[] args) {
+        new RuleProcessor().sayHello();
+    }
+}
